49df710eacff13ffc4299b7c644f5a84458bd175
George Geiwitz Cesana ne Guy <georgegeiwitzcesana@gmail.com>
2021-01-08 21:35:27 -0800

    Implemented Graph::forEachNode().

---
+++ src/GraphQuery.h
@@ -0,0 +1,16 @@
+#ifndef GRANKY_GRAPH_QUERY_H
+#define GRANKY_GRAPH_QUERY_H
+
+#include "Graph.h"
+
+namespace granky {
+
+class GraphQuery
+
+    Graph::Instance graph;
+    
+public:
+    GraphQuery() {};
+}; // namespace granky
+
+#endif // GRANKY_GRAPH_QUERY
